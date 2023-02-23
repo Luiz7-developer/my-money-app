93194b7a58a721ea674e98fb068e252b620f0b7c
Luiz Paulo <113642256+Luiz7-developer@users.noreply.github.com>
2023-02-23 14:33:57 -0300

    tabReducres

--- src/main/reducers.js
+++ src/main/reducers.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import DashboardReducer from "../dashboard/dashboardReducer";
+import TabReducers from "../common/tab/tabReducers";
 
 const rootReducers = combineReducers({
   dashboard: DashboardReducer,
+  tab: TabReducers,
 });
 
 export default rootReducers;
