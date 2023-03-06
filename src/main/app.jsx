673165cd4699ebc70d7509e57cda513ad5b4fa05
Luiz Paulo <113642256+Luiz7-developer@users.noreply.github.com>
2023-03-06 11:28:08 -0300

    Exibindo menssagens de error

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -4,6 +4,7 @@ import Header from "../common/template/header";
 import Sidebar from "../common/template/sidebar";
 import Footer from "../common/template/footer";
 import Routes from "./routes";
+import Messagens from "../common/msg/messagens";
 
 export default (props) => (
   <div className="wrapper">
@@ -13,5 +14,6 @@ export default (props) => (
       <Routes />
     </div>
     <Footer />
+    <Messagens />
   </div>
 );
--- src/main/reducers.js
+++ src/main/reducers.js
@@ -3,12 +3,14 @@ import DashboardReducer from "../dashboard/dashboardReducer";
 import TabReducers from "../common/tab/tabReducers";
 import BillingCyclesReducer from "../billingCycle/billingCyclesReducer";
 import { reducer as formReducer } from "redux-form";
+import { reducer as toastrReducer } from "react-redux-toastr";
 
 const rootReducers = combineReducers({
   dashboard: DashboardReducer,
   tab: TabReducers,
   billingCycle: BillingCyclesReducer,
   form: formReducer,
+  toastr: toastrReducer,
 });
 
 export default rootReducers;
